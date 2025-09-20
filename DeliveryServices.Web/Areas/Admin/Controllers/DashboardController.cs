f8ca84e47ea6952dda1276f3c6271f9c8abbbd27
Edris-01 <edris.mughulzai@gmail.com>
2025-09-20 20:53:26 +0500

    Add project files.

---
+++ DeliveryServices.Web/Areas/Admin/Controllers/DashboardController.cs
@@ -0,0 +1,19 @@
+﻿using DeliveryServices.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace DeliveryServices.Web.Areas.Admin.Controllers
+{
+    public class DashboardController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
